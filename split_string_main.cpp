60ffb2c35665167bd222d7a7f7dea6d27f83bbd7
aneury perez <aneury1@gmail.com>
2017-03-20 12:05:36 -0400

    adding a deleter function.
    adding a deleter function , string array created with new operator

--- split_string_main.cpp
+++ split_string_main.cpp
@@ -1,7 +1,6 @@
 
 
 
-
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -140,7 +139,7 @@ char **splitString2(char *str, char token)
        {
     	   int diference = len - current_char;
     	   ret[current_str+1] = new char[diference + 1];
-    	   char *other_temp = new char[diference + 1];
+    	   char *other_temp    = new char[diference + 1];
     	   getSubString(other_temp  , str, current_char, len);
     	   if(other_temp != NULL)
     	   {
@@ -173,6 +172,36 @@ int count_string_quantity_until_null(char **str)
 
 }
 
+void DeleteAllString(char **&str)
+{
+	   int iter =0;
+	   if(str == NULL)return;
+	   int c= count_string_quantity_until_null(str);
+	   for( ;iter< c; iter++)
+	   {
+		   if(str[iter] == NULL)break;
+		   {
+			   cout << "delete String # "<< iter << " is : "<< str[iter]<<endl;
+		       delete []str[iter];
+		       str[iter] =NULL;
+		   }
+	   }
+	   if(str[iter+1]!= NULL )
+	   {
+		   cout << "DELETE String # "<< iter +1 << " is : "<< str[iter+1]<<endl;
+	       delete []str[iter+1];
+	       str[iter] =NULL;
+	   }
+
+     delete str;
+     str = NULL;
+}
+
+bool TestIfNull(char **str){
+	return (str==NULL);
+}
+
+
 void PrintAllStr(char **str)
 {
    int iter =0;
@@ -184,7 +213,9 @@ void PrintAllStr(char **str)
 	     cout << "String # "<< iter << " is : "<< str[iter]<<endl;
    }
    if(str[iter+1]!= NULL )
+   {
 	   cout << "String # "<< iter +1 << " is : "<< str[iter+1]<<endl;
+   }
 }
 };///end namespace;
 
@@ -205,158 +236,10 @@ int main(int argc, char *argv[])
    cout <<"Argv[0] composed : "<< argv[0]<<endl;
    char **tk1 =  ASplitString::splitString2(argv[0], '\\');
    ASplitString::PrintAllStr(tk1);
- //  delete argv_0;
- //  argv_0 = NULL;
-   return 0;
-}
-
-       {
-    	    if(current_char == len)
-                 break;
-
-    	    if(*dup == token)
-    	    {
-                char* temp = new char[current_char - beg];
-
-                MidString(temp , str, beg , current_char);
-    	    	if(temp == NULL)
-    	    	{
-    	    		break;
-    	    	}
-
-    	    	int current_str_len = strlen(temp);
-
-                ret[current_str] = new char[current_str_len + 1];
-
-                strcpy(ret[current_str], temp);
-
-                cout << ret[current_str]<<endl;
-                current_str++;
-    	    	beg = current_char+1;
-    	        delete temp;
-    	        temp = NULL;
-    	    }
-    	    	current_char++;
-                dup++;
-       }
-   }
-   delete dup;
-   return ret;
-}
+   ASplitString::DeleteAllString(tk1);
+   cout << boolalpha <<ASplitString::TestIfNull(tk1);
 
 
-char **splitString2(char *str, char token)
-{
-   char *dup   = strdup2(str);
-   char **ret  = NULL;
-   int    len  = 0;
-   int  ch_cnt = 0;
-   int  current_char = 0;
-   int  current_str  = 0;
-   int  beg = 0;
-   if(str!= NULL)
-   {
-	   len    = strlen(str);
-	   ch_cnt = count_occuences(str, token);
-       ret    = new char *[ch_cnt + 1 + 1];
-       for(;;)
-       {
-    	    if(current_str == ch_cnt)
-    	    {
-    	    	break;
-    	    }
-    	    if(current_char == len)
-    	    {
-    	    	break;
-    	    }
-    	    if(*dup == token)
-    	    {
-                char* temp = new char[current_char - beg];
-                MidString(temp , str, beg , current_char);
-    	    	if(temp == NULL)
-    	    	{
-    	    		break;
-    	    	}
-    	    	int current_str_len = strlen(temp);
-                ret[current_str] = new char[current_str_len + 1];
-                strcpy(ret[current_str], temp);
-                current_str++;
-    	    	beg = current_char+1;
-    	        delete temp;
-    	        temp = NULL;
-    	    }
-    	    	current_char++;
-                dup++;
-       }
-       if(current_char < len)
-       {
-    	   int diference = len - current_char;
-    	   ret[current_str+1] = new char[diference + 1];
-    	   char *other_temp = new char[diference + 1];
-    	   MidString(other_temp  , str, current_char, len);
-    	   if(other_temp != NULL)
-    	   {
-    		   strcpy(ret[current_str+1], other_temp  );
-    		   if(ret[current_str+1] == NULL)
-    		   {
-
-    		   }
-    	   }
-       }
-       else{
-    	   ret[ch_cnt + 1]=NULL;
-          }
-   }
-   ret[ch_cnt + 1 + 1] = NULL;
-   delete dup;
-   return ret;
-}
-
-int count_string_quantity_until_null(char **str)
-{
-   int cnt = 0;
-   do{
-	   if(str[cnt] == NULL)
-		   break;
-	   else
-		   cnt++;
-   }while(1);
-   return cnt;
-
-}
-
-void PrintAllStr(char **str)
-{
-   int iter =0;
-   if(str == NULL)return;
-   int c= count_string_quantity_until_null(str);
-   for( ;iter< c; iter++)
-   {
-	   if(str[iter] == NULL)break;
-	     cout << "String # "<< iter << " is : "<< str[iter]<<endl;
-   }
-   if(str[iter+1]!= NULL )
-	   cout << "String # "<< iter +1 << " is : "<< str[iter+1]<<endl;
-}
-};///end namespace;
-
-int main(int argc, char *argv[])
-{
-  // char *argv_0 = ASplitString::strdup2(argv[0]);
-
-  // cout << argv_0 <<endl;
-
-  // char *mid= new char[22];
-//
- //  ASplitString::MidString(mid, argv_0, 0, 6);
-  // cout << mid <<endl;
-
-  // int o = ASplitString::count_occuences(argv[0], '\\');
-
-//   cout << o;
-   cout <<"Argv[0] composed : "<< argv[0]<<endl;
-   char **tk1 =  ASplitString::splitString2(argv[0], '\\');
-   ASplitString::PrintAllStr(tk1);
  //  delete argv_0;
  //  argv_0 = NULL;
    return 0;
